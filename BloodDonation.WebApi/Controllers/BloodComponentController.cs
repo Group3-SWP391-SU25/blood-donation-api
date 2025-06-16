0f67f475b31aa259e4b609cd12e4c1a847e55cd4
BanBannBannn <tranvangiaban@gmail.com>
2025-06-16 18:50:00 +0700

    deploy api, db, add api bloodcomponent

---
+++ BloodDonation.WebApi/Controllers/BloodComponentController.cs
@@ -0,0 +1,32 @@
+﻿using BloodDonation.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.WebApi.Controllers
+{
+    [Route("api/blood-components")]
+    [ApiController]
+    public class BloodComponentController : ControllerBase
+    {
+        private readonly IBloodComponentService bloodComponentService;
+        private readonly IClaimsService ClaimsService;
+        public BloodComponentController(IBloodComponentService bloodComponentService, IClaimsService claimsService)
+        {
+            this.bloodComponentService = bloodComponentService;
+            ClaimsService = claimsService;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> Get([FromQuery] string? search = "")
+        {
+            try
+            {
+                var result = await bloodComponentService.GetAllAsync(search);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Lỗi hệ thống", detail = ex.Message });
+            }
+        }
+    }
+}
