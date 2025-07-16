e610daf0444a09072993089f565ffb28b023c63a
tannt-tv <tannt19092001@gmail.com>
2025-07-16 21:58:40 +0700

    fix: remove default value

--- BloodDonation.Application/Models/Users/UserUpdateModel.cs
+++ BloodDonation.Application/Models/Users/UserUpdateModel.cs
@@ -3,13 +3,13 @@
 public class UserUpdateModel
 {
     public DateTime? DateOfBirth { get; set; }
-    public string? FullName { get; set; } = string.Empty;
+    public string? FullName { get; set; }
 
-    public string? PhoneNo { get; set; } = string.Empty;
+    public string? PhoneNo { get; set; }
     public bool? Gender { get; set; }
-    public string? Addresss { get; set; } = string.Empty;
-    public string? FrontUrlIdentity { get; set; } = string.Empty;
-    public string? BackUrlIdentity { get; set; } = string.Empty;
+    public string? Addresss { get; set; }
+    public string? FrontUrlIdentity { get; set; }
+    public string? BackUrlIdentity { get; set; }
     public Guid? RoleId { get; set; }
     public string? Status { get; set; }
     public Guid? BloodGroupId { get; set; }
